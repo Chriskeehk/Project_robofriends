0c60e1849fcb9eb9bbdf1d27f3b324a419cf5c2a
Chris Ho <63154337+Chriskeehk@users.noreply.github.com>
2020-06-17 21:51:18 +0800

    Create OO_Class.js

---
+++ OO_Class.js
@@ -0,0 +1,41 @@
+
+========
+Class House 
+table: object
+chair: object
+Playing_Music : Function
+
+Fun Playing_Music ()
+{
+
+}
+========
+Class House_22 extends House
+Playing_TV   : Function
+Fun Playing_TV ()
+{
+}
+
+========
+var_myclass = House init;
+var_myclass.table = 3;  (Propertie)
+var_myclass.chair = green;
+var_myclass.Playing_Music();
+
+var_myclass2 = House init;
+var_myclass2.table = 44;
+var_myclass2.chair = blue;
+var_myclass2.Playing_Music(a,2);
+
+var_myclass3 = House_22 init;
+var_myclass3.table = 77;
+var_myclass3.Playing_TV ()
+
+Class ( Function , hard/foot)
+A(123,var_aa) ---> B (var_1, var_2)
+{
+}
+
+static FunName () => {
+
+}
